6cab34235b5d425ffcc40b661c9e86a5c157e9ff
mahme <mustafasaleh.hf@gmail.com>
2021-07-31 14:37:09 +0200

    set file structure, prettier, eslint, husky, bootstrap and emotion

---
+++ scripts/pre-push.js
@@ -0,0 +1,6 @@
+var spawnSync = require('child_process').spawnSync
+
+const result = spawnSync('npm run validate', {stdio: 'inherit', shell: true})
+if (result.status !== 0) {
+  process.exit(result.status)
+}
